38a96f8d320be3af970089b099f6e4aacfd21e34
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-08-04 17:57:49 -0600

    header page

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,40 @@
+const Header = () => {
+    return (
+        <div className="border-b border-(--border-color)">
+            <div className="container  px-4 py-4 m-auto flex justify-between" >
+                <div className="flex">
+                    <div>
+                        <div className="bg-[#2563EB] rounded-[100%] w-[3rem] h-[3rem] flex justify-center items-center">
+                            <p className="m-0">B</p>
+                        </div>
+                    </div>
+                    <div className="ml-4">
+                        <p className="font-bold text-lg ">Entre Nosotros</p>
+                        <p className="text-sm text-(--secondary-color)">Escrito por amigos, para lectores. Conoce nuestras historias y pasiones.</p>
+                    </div>
+                </div>
+                <div className="flex items-center">
+                    <ul className="flex text-sm font-bold text-(--menu-color)">
+                        <p>
+                            Home
+                        </p>
+                        <p className="ml-4">
+                            Autores
+                        </p>
+                        <p className="ml-4">
+                            Categorias
+                        </p>
+                        <p className="ml-4">
+                            Acerda de
+                        </p>
+                        <p className="ml-4">
+                            Contacto
+                        </p>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
