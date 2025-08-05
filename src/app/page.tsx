bcbe3a2a679ac93ea1cde252df4d95040fb861e0
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-08-04 18:27:06 -0600

    page Header Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hola Mundo</h1>
+    <div className=" container m-auto text-center">
+      <h1 className="font-bold text-5xl mt-11 mb-6">Comunidad de Creadores</h1>
+      <div className="w-[65%] m-auto">
+        <p className="text-(--secondary-color) font-bold text-lg">Una colección de historias, tutoriales e ideas de apasionados creadores en diversos campos. Desde la tecnología hasta el arte, la sostenibilidad y la cocina. Descubre nuevas perspectivas.</p>
+      </div>
+    </div>
   );
 }
