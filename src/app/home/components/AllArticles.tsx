295fd49ac38f9be4a8ff214b35daa4322502edcb
GuillermoSM5 <guillermosotomarquez5@gmail.com>
2025-08-06 11:01:56 -0600

    feat(horizontalpostcard): create a tag for horizontal post card

--- src/app/home/components/AllArticles.tsx
+++ src/app/home/components/AllArticles.tsx
@@ -3,6 +3,7 @@ import HorizontalPostCard from "./HorizontalPostCard";
 const AllArticles = () => {
     return (
         <div className="container m-auto mt-12">
+            <h2 className="font-black text-2xl mb-8">Ultimos Articulos</h2>
             <HorizontalPostCard />
         </div>
     );
--- src/app/home/components/HorizontalPostCard.tsx
+++ src/app/home/components/HorizontalPostCard.tsx
@@ -1,8 +1,23 @@
+import Tag from "@/app/components/Tag";
+
 const HorizontalPostCard = () => {
+
+    const tags = ['Web Develoment', 'Next.js', 'Next.js', 'Next.js', 'Next.js']
+
     return (
-        <>
-            <h1>Horizontal</h1>
-        </>
+        <div className="border border-(--border-color) w-sm px-6 rounded-2xl">
+            <div></div>
+            <div className="py-6 ">
+                <div className="flex [&>*]:mr-2 [&>*]:mb-2 flex-wrap">
+                    {
+                        tags.map((text, index) => <Tag text={text} key={index} />)
+                    }
+                </div>
+                <p>
+                    Building Scalable Web Applications with Next.js and TypeScript
+                </p>
+            </div>
+        </div>
     );
 }
 
